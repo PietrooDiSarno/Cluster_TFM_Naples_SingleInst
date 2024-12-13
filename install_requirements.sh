862811a14f5af0f8f2da250465c284dd5f59a311
Pablo Torres <133156287+Pablo-TR@users.noreply.github.com>
2024-12-13 19:22:15 +0100

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -5,6 +5,7 @@ pip3 install -r requirements.txt
 #LLOP packages
 #pySPICElib @ git+https://github.com/ManelSoria/pySPICElib.git
 pip3 install git+ssh://git@github.com/ManelSoria/pySPICElib
+pip3 install git+ssh://git@github.com/PauBet/PSOA@test/pablo_oplan1
 
 # Local repo as editable module (for development and execution from terminal)
 pip install -e ./
